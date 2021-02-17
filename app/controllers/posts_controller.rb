83383e303fc5a3aef5e0d28e95db3e304727014d
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-02-17 21:00:12 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,20 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(id: "DESC")  # 全てのレコードを@postに代入 id: "DESC"で新しい情報が1番上に表示されるようにする
+    @posts = Post.all.order(id: "DESC")
   end
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-
   def checked
-    post = post.find(params[:id])
+    post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
-    end    
-
-    item = post.find(params)
-    rendder json: { post: item}
+    end
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
+
 end
