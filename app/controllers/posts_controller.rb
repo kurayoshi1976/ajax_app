f9402a13734bb6a93305c9161a662dcf4071832a
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-02-17 15:27:59 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # 全てのレコードを@postに代入
+    @posts = Post.all.order(id: "DESC")  # 全てのレコードを@postに代入 id: "DESC"で新しい情報が1番上に表示されるようにする
   end
 
-  def new
-  end 
-  
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
